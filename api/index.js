2405fe6197a67c7bc5600512197fa1ce3212522a
Anton Loiko <metallist9656@gmail.com>
2021-05-08 23:29:04 +0300

    Add dotenv

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
 const { Telegraf } = require("telegraf")
-console.log(process.env.BOT_TOKEN, "<=== process.env.BOT_TOKEN ===")
+require("dotenv").config()
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.command("oldschool", (ctx) => ctx.reply("Hello"))
 bot.command("hipster", Telegraf.reply("λ"))
-bot.launch()
-
-// Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"))
-process.once("SIGTERM", () => bot.stop("SIGTERM"))
+// bot.launch()
 
 module.exports = (req, res) => {
   bot.launch()
 
+  // Enable graceful stop
+  process.once("SIGINT", () => bot.stop("SIGINT"))
+  process.once("SIGTERM", () => bot.stop("SIGTERM"))
   res.json(true)
 }
