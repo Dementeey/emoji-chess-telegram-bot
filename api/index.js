668fd1733e9849fbb959c88312b1ceb42392b8f6
Anton Loiko <metallist9656@gmail.com>
2021-05-08 23:14:04 +0300

    second commit

--- api/index.js
+++ api/index.js
@@ -8,3 +8,9 @@ bot.launch()
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"))
 process.once("SIGTERM", () => bot.stop("SIGTERM"))
+
+module.exports = (req, res) => {
+  bot.launch()
+
+  res.json(true)
+}
